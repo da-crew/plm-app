9ca001b727a7bf6b30e88f978104f1fc4cbcb254
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-10-30 15:36:08 +0700

    remove unused imports

--- plm-server/src/main/java/com/studentgroup/app/webservices/UserController.java
+++ plm-server/src/main/java/com/studentgroup/app/webservices/UserController.java
@@ -2,21 +2,16 @@ package com.studentgroup.app.webservices;
 
 import java.security.NoSuchAlgorithmException;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.studentgroup.app.model.EmployeeUser;
