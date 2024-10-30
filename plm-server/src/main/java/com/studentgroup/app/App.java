d132cf8df550b820e95e24d4d52201292a778ab4
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-10-30 15:37:03 +0700

    remove unused imports

--- plm-server/src/main/java/com/studentgroup/app/App.java
+++ plm-server/src/main/java/com/studentgroup/app/App.java
@@ -2,15 +2,10 @@ package com.studentgroup.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.studentgroup.app.model.EmployeeUser;
-import com.studentgroup.app.model.Role;
 
 @SpringBootApplication
 public class App {
--- plm-server/src/main/java/com/studentgroup/app/model/UserRepository.java
+++ plm-server/src/main/java/com/studentgroup/app/model/UserRepository.java
@@ -2,7 +2,6 @@ package com.studentgroup.app.model;
 
 import org.springframework.data.repository.*;
 import org.springframework.stereotype.Repository;
-import java.util.*;
 
 @Repository
 public interface UserRepository extends CrudRepository<EmployeeUser, Long> {
