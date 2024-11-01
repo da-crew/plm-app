f0b78761e7ae212df1e91670392b5cbfe5558add
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-11-01 14:29:21 +0700

    implement the login system and add a dashboard

--- plm-app/src/routes/Dashboard.jsx
+++ plm-app/src/routes/Dashboard.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { Navigate } from "react-router";
+import { authToken } from "../users";
 
 
 export default function Dashboard() {
-    return <h1>This is a dashboard</h1>
+    let [cookies, setCookies, removeCookies] = useCookies(['loginToken']);
+    let [toLogin, setToLogin] = useState(false);
+
+    useEffect(() => {
+        if (cookies.loginToken != null) {
+            authToken(cookies.loginToken)
+                .then((value) => {})
+                .catch((e) => {
+                    console.log("Invalid token");
+                    setCookies("loginCookies", null);
+                    setToLogin(true);
+                });
+        } else {
+            console.log("Invalid token");
+            setToLogin(true);
+        }
+    }, []);
+
+    if (toLogin) {
+        return <Navigate to="/login"/>
+    }
+
+
+    return (<>
+        <h1>This is a dashboard</h1>
+        
+    </>);
 }
--- plm-app/src/routes/Login.jsx
+++ plm-app/src/routes/Login.jsx
@@ -1,16 +1,53 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 import { Navigate, redirect } from "react-router";
+import { authenticate, authToken, COOKIES_NAME } from "../users";
 
 
 export default function Login() {
-    useEffect(() => {
-        console.log("Effect");
-        redirect("/testing");
-    });
-
     let [username, setUsername] = useState("");
     let [password, setPassword] = useState("");
+    let [loginFailed, setLoginFailed] = useState(false);
+    let [isRedirect, setRedirect] = useState(false);
 
+    let [cookies, setCookies, removeCookies] = useCookies([COOKIES_NAME]);
     
-    return <h1>Login Now!</h1>;
+
+    function submitCreds() {
+        authenticate(username, password)
+            .then((value) => {
+                setCookies(COOKIES_NAME, value.data.token);
+                setRedirect(true);
+            })
+            .catch((err) => {
+                setLoginFailed(true);
+            });
+    }
+
+    useEffect(() => {
+        if (cookies.loginToken != null) {
+            authToken(cookies.loginToken)
+                .then((value) => {})
+                .catch((e) => {
+                    setCookies(COOKIES_NAME, null);
+                });
+        }
+    }, []);
+
+    if (isRedirect) {
+        return <Navigate to="/"/>
+    }
+
+
+    return (<>
+        <h2>Login</h2>
+        {loginFailed ? <p>Username or password is incorrect!</p> : <p></p>}
+        <form>
+            <label htmlFor="username">Username: </label>
+            <input type="text" id="username" onChange={(e) => setUsername(e.target.value)}></input><br/>
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}></input>
+        </form>
+        <button onClick={() => {submitCreds()}}>Login</button>
+    </>);
 }
