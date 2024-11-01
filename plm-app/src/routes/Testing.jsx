13f7e4ad1a1b152b57df041ce0e53857b08fc466
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-11-01 14:29:26 +0700

    Update Testing.jsx

--- plm-app/src/routes/Testing.jsx
+++ plm-app/src/routes/Testing.jsx
@@ -1,49 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
-import { authenticate } from "../users";
+import { authenticate, authToken, COOKIES_NAME } from "../users";
 
 export default function Testing() {
 
     let [username, setUsername] = useState("");
     let [password, setPassword] = useState("");
+
+    let [displayName, setDisplayName] = useState("");
+
     let [isLogin, setLogin] = useState(false);
-    let [cookies, setCookies, removeCookies] = useCookies(['loginToken']);
-    let [token, setToken] = useState("");
-
-    /*
-    setCookies('loginToken', "Password here");
+    let [loginFailed, setLoginFailed] = useState(false);
+    let [cookies, setCookies, removeCookies] = useCookies([COOKIES_NAME]);
+    let [token, setToken] = useState(null);
     
-    const authUrl = 'http://localhost:8080/users/auth';
-
-    console.log("cookies: " + cookies.loginToken);
-    
-    function auth() {
-        if (username === "" || password === "")
-            return;
-        console.log("Sending a request");
-
-        let data = {
-            params: {
-                username: username,
-                password: password
-            },
-        };
-
-        axios.get(authUrl, data)
+    if (cookies.loginToken != null) {
+        authToken(cookies.loginToken)
             .then((value) => {
-                console.log(`success: ${JSON.stringify(value.data.token)} \nstatus: ${value.status}`);
-                setToken(value.data.token);
-            })
-            .catch((error) => console.log("error " + error));
+                setToken(cookies.loginToken);
+                setLogin(true);
+                setDisplayName(value.data.username);
+                console.log("Login successful");
+            }).catch((e) => {
+                setCookies(COOKIES_NAME, null);
+            });
     }
-    */
+
+
 
     return (<>
         <h2>Testing Page</h2>
+        <p>{loginFailed ? "Login failed" : ""}</p>
         {
             isLogin ? <div>
-                <p>Welcome, {username}</p>
+                <p>Welcome, {displayName}</p>
                 <p>Login token: {token}</p>
             </div> : <p>Please Login.</p>
         }
@@ -55,7 +46,19 @@ export default function Testing() {
             <input type="button" value="Submit" onClick={(e) => {
                 e.preventDefault();
                 console.log("authenticating");
-                authenticate(username, password);
+                authenticate(username, password)
+                    .then((value) => {
+                        setToken(value.data.token);
+                        setCookies("loginToken", value.data.token);
+                        setLoginFailed(false);
+                        setDisplayName(username);
+                        setLogin(true);
+                        console.log("Login successful");
+                    }
+                    ).catch((error) => {
+                        setLoginFailed(true);
+                        console.log("Login failed");
+                    });
             }}></input>
         </form>
 
@@ -75,6 +78,11 @@ export default function Testing() {
             axios.get("http://localhost:8080/users/role", { params: { token: token } })
                 .then((value) => console.log(`success: ${JSON.stringify(value.data)} \nstatus: ${value.status}`))
                 .catch((error) => console.log("error " + error));
-        }}>Check role</button>
+        }}>Check role</button><br/>
+        <button onClick={() => {
+            setCookies("loginToken", null);
+        }}>
+        Clear Cookies
+        </button>
     </>);
 }
