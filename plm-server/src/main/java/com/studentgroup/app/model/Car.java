0b384dd78c27aef1c40d9e7f3b5be3b7ee6fe059
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-11-05 22:18:43 +0700

    add tables

--- plm-server/src/main/java/com/studentgroup/app/model/Car.java
+++ plm-server/src/main/java/com/studentgroup/app/model/Car.java
@@ -1,5 +1,17 @@
 package com.studentgroup.app.model;
 
+import jakarta.persistence.*;
+
+
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     
+    @Column(name = "CAR_MODEL")
+    String modelName;
+
+    @OneToOne
+    Report report;
 }
--- plm-server/src/main/java/com/studentgroup/app/model/Report.java
+++ plm-server/src/main/java/com/studentgroup/app/model/Report.java
@@ -1,5 +1,28 @@
 package com.studentgroup.app.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "report")
 public class Report {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //1-1 to CAR_ID
+    @OneToOne
+    Car car;
+
+    @Lob
+    @Column(name = "damage_report")
+    String reportText;
 }
