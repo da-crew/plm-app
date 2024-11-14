eea2c9c859f2536cc0cc03fb08fc429ce89b7425
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-11-14 16:45:10 +0700

    Revert "frghj"
    This reverts commit 729fd422503447c3fd27e2886ff6fe03d4c9668c.

--- plm-server/src/main/java/com/studentgroup/app/App.java
+++ plm-server/src/main/java/com/studentgroup/app/App.java
@@ -1,22 +1,14 @@
 package com.studentgroup.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.studentgroup.app.services.FilesStorageService;
-
-import jakarta.annotation.Resource;
-
 
 @SpringBootApplication
-public class App implements CommandLineRunner{
-
-    @Resource
-    FilesStorageService storageService;
+public class App {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
@@ -33,9 +25,4 @@ public class App implements CommandLineRunner{
 			}
 		};
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        storageService.init();
-    }
 }
--- plm-server/src/main/java/com/studentgroup/app/webservices/ProductOrderController.java
+++ plm-server/src/main/java/com/studentgroup/app/webservices/ProductOrderController.java
@@ -1,7 +1,6 @@
 package com.studentgroup.app.webservices;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studentgroup.app.model.*;
@@ -11,9 +10,6 @@ import com.studentgroup.app.model.repositories.ProductOrderRepository;
 import com.studentgroup.app.model.repositories.UserRepository;
 import com.studentgroup.app.webservices.authorization.*;
 import com.studentgroup.app.webservices.authorization.AuthorizationResult;
-
-import jakarta.servlet.http.HttpServletRequest;
-
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
@@ -25,7 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class ProductOrderController {
@@ -106,8 +101,8 @@ public class ProductOrderController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/product-orders/upload")
-    public String postMethodName(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+    @PostMapping("/product-orders/{bl-number}/set-image")
+    public String postMethodName(@PathVariable String blNumber) {
         //TODO: process POST request
         
         return "entity";
