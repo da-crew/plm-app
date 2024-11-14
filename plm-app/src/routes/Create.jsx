dcbeec7ff0d45909729ce5e6d9496d30126aaee0
Aphichai-Dancharoenrak <157001482+aphichaidancharoenrak@users.noreply.github.com>
2024-11-14 14:47:28 +0700

    Update Create.jsx

--- plm-app/src/routes/Create.jsx
+++ plm-app/src/routes/Create.jsx
@@ -2,31 +2,29 @@ import { useEffect, useState } from "react";
 import { Navigate } from "react-router";
 import Header from "../components/Header";
 import DashboardHeader from "../components/DashboardHeader";
-import { authenticate, COOKIES_NAME, Role, useAuthenticate } from "../users";
 import ProductOrder from "../components/ProductOrder";
+import { authenticate, COOKIES_NAME, Role, useAuthenticate } from "../users";
 
 export default function Create() {
     let [toLogin, setToLogin] = useState(false);
-    let [validCreds, userInfo, password] = useAuthenticate();
+    let [validCreds, userInfo] = useAuthenticate();
 
-    useEffect(() => console.log(JSON.stringify(userInfo)), []);
+    useEffect(() => console.log(JSON.stringify(userInfo)), [userInfo]);
+
     if (toLogin || !validCreds) {
-        return <Navigate to="/login" />
+        return <Navigate to="/login" />;
     }
 
-    return (<>
-        <Header employeeName={userInfo.username} onLogout={() => setToLogin(true)} role={userInfo.role} />
-        <h1>This is a Create</h1>
-        <p>Welcome, {userInfo.username}</p>
-        <p>Role: {Role.toString(userInfo.role)}</p>
+    return (
+        <>
+            <Header employeeName={userInfo.username} onLogout={() => setToLogin(true)} role={userInfo.role} />
+            <DashboardHeader />
+            <h1>This is a Create</h1>
+            <p>Welcome, {userInfo.username}</p>
+            <p>Role: {Role.toString(userInfo.role)}</p>
 
-
-        <ProductOrder> {/* use ProductOrder component */}</>
-    
-        
-        
-
-    </>
+            <ProductOrder /> {/* Use ProductOrder component */}
+        </>
     );
 
 
