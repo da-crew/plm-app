f391f033f1ec11921154dc0453c8983520a6746d
PokpongRomsri <149413924+PokpongRomsri@users.noreply.github.com>
2024-11-15 23:06:31 +0700

    nothing

--- plm-app/src/components/ProductOrder/loadDetails.jsx
+++ plm-app/src/components/ProductOrder/loadDetails.jsx
@@ -1,42 +1,4 @@
-// import React, { useState } from 'react';
-// import './ViewProductOrder.css';
 
-
-// function LoadDetails({ params, productOrders }) {
-//     // Find the product order with the matching BL Number
-//     const thisProductOrder = productOrders.find(
-//         (item) => item.blnumber === params
-//     );
-
-//     // If no matching product order is found
-//     if (!thisProductOrder) {
-//         return <p>No matching product order found.</p>;
-//     }
-
-//     let [cars, setCars] = useState([])
-//     let [trucks, setTrucks] = useState([])
-//     // productOrders.map((car[key, index]))
-//     for (let i = 0; i < thisProductOrder.cars.length; i++) {
-//         if (!cars.includes(thisProductOrder.cars[i])) { setCars = [previousCars + thisProductOrder.cars[i]] }
-//         if (!trucks.includes(thisProductOrder.cars[i].truck)) { setTrucks = [previousTruck + thisProductOrder.cars[i].truck] }
-//     }
-
-//     return (
-//         <div className="load-details">
-//             <h4>Load Details</h4>
-
-//             {
-//                 expected output:
-
-//                 truck: TRK-1023
-//                 contains i: 5 , 6 , 7
-//             }
-
-//         </div>
-//     );
-// }
-
-// export default LoadDetails;
 import React, { useState, useEffect } from 'react';
 import './ViewProductOrder.css';
 
