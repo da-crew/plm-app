62c1cedfbfbafb483e599922331e81ba6ca12910
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2024-11-17 01:20:27 +0700

    add role checking in product order editing

--- plm-server/src/main/java/com/studentgroup/app/webservices/ProductOrderController.java
+++ plm-server/src/main/java/com/studentgroup/app/webservices/ProductOrderController.java
@@ -356,7 +356,11 @@ public class ProductOrderController {
 
         EmployeeUser caller = authRes.getUser();
 
+
         if (newChecker != null && !productOrder.getChecker().getUsername().equals(newCheckerUsername)) {
+            if (newChecker.getRole() != Role.ADMIN && newChecker.getRole() != Role.CHECKER)
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("assigned checker must be a checker or an admin!");
+
             productOrder.removeChecker();
             newChecker.assignAsChecker(productOrder);
             ActionLog actionLog = new ActionLog("Reassign to " + newChecker.getUsername() + " as a checker");
@@ -364,6 +368,8 @@ public class ProductOrderController {
         }
 
         if (newDispatcher != null && !productOrder.getDispatcher().getUsername().equals(newDispatcherUsername)) {
+            if (newDispatcher.getRole() != Role.ADMIN && newDispatcher.getRole() != Role.DISPATCHER)
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("assigned dispatcher must be a dispatcher or an admin!");
             productOrder.removeDispatcher();
             newDispatcher.assignAsDispatcher(productOrder);
             ActionLog actionLog = new ActionLog("Reassign to " + newDispatcher.getUsername() + " as a dispatcher");
